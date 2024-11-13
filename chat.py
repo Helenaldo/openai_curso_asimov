f54f8f28549e95c16ae7177817113c315603ce91
Helenaldo <contato@helenaldo.com.br>
2024-11-13 16:05:46 -0300

    chat do Helenaldo

---
+++ chat.py
@@ -0,0 +1,35 @@
+import openai
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+client = openai.Client()
+
+def geracao_texto(mensagens):
+    resposta = client.chat.completions.create(
+        model='gpt-3.5-turbo-0125',
+        messages=mensagens,
+        temperature=0,
+        max_tokens=1000,
+        stream=True
+    )
+    
+    print('Assistant: ', end='')
+    texto_completo = ''
+
+    for resposta_stream in resposta:
+        texto = resposta_stream.choices[0].delta.content
+        if texto:
+            print(texto, end='')
+            texto_completo += texto
+    print()
+    
+    mensagens.append({'role': 'assistant', 'content': texto_completo})
+    return mensagens
+
+if __name__ == '__main__':
+    print('Bem-vindo ao chatBot do Helenaldo')
+    mensagens = []
+    while True:
+        input_usuario = input('User: ')
+        mensagens.append({'role': 'user', 'content': input_usuario})
+        mensagens = geracao_texto(mensagens)
